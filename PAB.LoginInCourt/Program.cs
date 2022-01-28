91228ae14c8815a530d0b3afcd23a9706b7968a2
giorgi nozadze <nozadzegiorgimoa@gmail.com>
2022-01-28 10:49:02 +0400

    test

--- PAB.LoginInCourt/Program.cs
+++ PAB.LoginInCourt/Program.cs
@@ -17,6 +17,6 @@ namespace PAB.LoginInCourt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
+        } 
     }
 }
